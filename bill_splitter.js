cdfa4664c0f8c1a400286dc817cfc6b125f33369
jenna0315 <jbellagamba@usf.edu>
2024-09-06 16:59:59 -0400

    Add output functionality to display bill, tip, and total

--- bill_splitter.js
+++ bill_splitter.js
@@ -2,3 +2,11 @@
 let customerBill = 295 //number
 let customerTip = (300 < customerBill < 50) ? (.2*customerBill) : (.15*customerBill) 
 console.log(customerBill, customerTip)
+
+//Add output functionality to display bill, tip, and total
+let customerTotal = customerBill+customerTip //number
+let customerSummary = `Customer Summary:
+The bill was $${customerBill} 
+The tip was $${customerTip}
+The total was $${customerTotal}`
+console.log(customerSummary)
