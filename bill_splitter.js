bae36828f1c587026a06e7c481949495a9b07c35
jenna0315 <jbellagamba@usf.edu>
2024-09-06 17:53:52 -0400

    changed customer tip variable

--- bill_splitter.js
+++ bill_splitter.js
@@ -1,13 +1,13 @@
 //Initial Logic for Calculating the Tip
 var customerBill = 295 //number
-var customerTip = (300 < customerBill < 50) ? (.2*customerBill) : (.15*customerBill) 
-console.log("Customer Tip:",customerTip)
+var customertip = (300 < customerBill < 50) ? (.2*customerBill) : (.15*customerBill) 
+console.log("Customer Tip:",customertip)
 
 //Add output functionality to display bill, tip, and total
-let customerTotal = customerBill+customerTip //number
+let customerTotal = customerBill+customertip //number
 let customerSummary = `Customer Summary:
 The bill was $${customerBill} 
-The tip was $${customerTip}
+The tip was $${customertip}
 The total was $${customerTotal}`
 console.log(customerSummary)
 
