6fbba0342549e58a89299bcf75d5a9977b8835db
jenna0315 <jbellagamba@usf.edu>
2024-09-06 17:50:34 -0400

    adjusted parethesis

--- bill_splitter.js
+++ bill_splitter.js
@@ -1,7 +1,7 @@
 //Initial Logic for Calculating the Tip
 var customerBill = 295 //number
 var customerTip = (300 < customerBill < 50) ? (.2*customerBill) : (.15*customerBill) 
-(console.log("Customer Tip:",customerTip))
+console.log("Customer Tip:",customerTip)
 
 //Add output functionality to display bill, tip, and total
 let customerTotal = customerBill+customerTip //number
